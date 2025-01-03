0f82bd4bf9ef7778f817d8179f87734f702fd73c
Malith Dilshan <IT21166556@my.sliit.lk>
2025-01-03 19:25:28 +0530

    changes done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+
+dotenv.config();
 export function registerUser(req, res) {
   const data = req.body;
 
