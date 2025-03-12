400279226b6b0f0c552957d4536752c558c0da9d
DilshanGAM <it21180934@my.sliit.lk>
2025-03-12 13:51:22 +0530

    day 21❤️

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { blockOrUnblockUser, getAllUsers, getUser, loginUser, registerUser } from "../controllers/userController.js";
+import { blockOrUnblockUser, getAllUsers, getUser, loginUser, loginWithGoogle, registerUser, sendOTP, verifyOTP } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -10,6 +10,12 @@ userRouter.post("/login",loginUser)
 userRouter.get("/all",getAllUsers)
 
 userRouter.put("/block/:email",blockOrUnblockUser)
+
+userRouter.post("/google",loginWithGoogle)
+
+userRouter.get("/sendOTP",sendOTP)
+
+userRouter.post("/verifyEmail",verifyOTP)
 
 userRouter.get("/",getUser)
 
